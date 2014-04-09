631a7faa8c0abb164296e149fe885cf7cd8f6b33
sIuv <pontus@englunds.de>
2014-04-09 16:06:10 +0200

    1

--- runJAutomateDemo.bat
+++ runJAutomateDemo.bat
@@ -1,3 +1,3 @@
 FOR /R "scripts/" %%f IN ("*.txt") DO (
- jre\bin\java.exe -Xmx512m -jar jautomate_xunit.jar localhost:1234 scripts/%~nxf
+ jre\bin\java.exe -Xmx512m -jar jautomate_xunit.jar localhost:1234 scripts/%%~nxf
 )
