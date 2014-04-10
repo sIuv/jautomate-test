523e6816ef6ebeea7f618baf4ac0dc04296801bd
sIuv <pontus@englunds.de>
2014-04-10 08:58:53 +0200

    Added comments

--- runJAutomateDemo.bat
+++ runJAutomateDemo.bat
@@ -1,3 +1,5 @@
+REM This file executes all .txt files in scripts/examples/
+
 FOR /R "scripts/examples/" %%f IN ("*.txt") DO (
  jre\bin\java.exe -Xmx512m -jar jautomate_xunit.jar localhost:1234 scripts/examples/%%~nxf
 )
