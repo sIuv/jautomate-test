132b52b63bd4c7c668c154574d922520f30e31a1
sIuv <pontus@englunds.de>
2014-04-10 15:11:56 +0200

    Diff

--- runJAutomateDemo.bat
+++ runJAutomateDemo.bat
@@ -1,5 +1,5 @@
 REM This file executes all .txt files in scripts/examples/
 
 FOR /R "scripts/JenkinsScript" %%f IN ("*.txt") DO (
- java -Xmx512m -jar jautomate_xunit.jar localhost:1234 scripts/JenkinsScript/%%~nxf
+ java -Xmx512m -jar jautomate_xunit.jar localhost:1234 workspace/scripts/JenkinsScript/%%~nxf
 )
