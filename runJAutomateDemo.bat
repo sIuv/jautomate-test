8eae548fde08a541ab87ed6b992ce6372f35c670
sIuv <pontus@englunds.de>
2014-04-10 11:09:35 +0200

    Remapping of search path

--- runJAutomateDemo.bat
+++ runJAutomateDemo.bat
@@ -1,5 +1,5 @@
 REM This file executes all .txt files in scripts/examples/
 
-FOR /R "scripts/examples/" %%f IN ("*.txt") DO (
- java -Xmx512m -jar jautomate_xunit.jar localhost:1234 scripts/examples/%%~nxf
+FOR /R "scripts/" %%f IN ("*.txt") DO (
+ java -Xmx512m -jar jautomate_xunit.jar localhost:1234 scripts/%%~nxf
 )
