33dab9baf15fd03e46054f8b15dccb971d4c0cab
sIuv <pontus@englunds.de>
2014-04-10 08:52:13 +0200

    Update runJAutomateDemo.bat
    magic johnson

--- runJAutomateDemo.bat
+++ runJAutomateDemo.bat
@@ -1,3 +1,3 @@
 FOR /R "scripts/examples/" %%f IN ("*.txt") DO (
- jre\bin\java.exe -Xmx512m -jar jautomate_xunit.jar localhost:1234 scripts/examples/%%~nxf
+ java -Xmx512m -jar jautomate_xunit.jar localhost:1234 scripts/examples/%%~nxf
 )
